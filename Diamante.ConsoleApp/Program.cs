5186133985f311b613f62d5019087762c4617cf4
nysalvi <nysalvi20@gmail.com>
2022-02-16 16:08:52 -0300

    Add project files.

---
+++ Diamante.ConsoleApp/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Diamante.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args) {
+            int x, contador = 0, metadeA, metadeB;
+            while (true) {
+                Console.WriteLine("Digite um valor que será utilizado para calcular o tamanho do diamante");
+                if (!int.TryParse(Console.ReadLine(), out x) || x % 2 == 0) {
+                    Console.WriteLine("Digite apenas números e com a condição de que sejam ímpares !!");
+                    continue;
+                }
+                break;
+            }
+            metadeA = x / 2 ;
+            metadeB = x / 2 ;
+            int parte2 = 0;
+            while (true) {
+                if (parte2 == 0) {
+                    if ( contador < metadeA || contador > metadeB ) {
+                        Console.Write(" ");
+                    } else {
+                        Console.Write("x");
+                    }
+                    if (metadeA == 0) {
+                        parte2++;
+                    }
+                    if (contador == x) {
+                        contador = 0;
+                        metadeA--;
+                        metadeB++;
+                        Console.WriteLine();
+                        continue;
+                        }
+                }
+                else {
+                    if (contador < metadeA || contador > metadeB) {
+                        Console.Write(" ");
+                    } else {
+                        Console.Write("x");
+                    }
+                    if (contador == x) {
+                        contador = 0;
+                        metadeA++;
+                        metadeB--;
+                        Console.WriteLine();
+                        continue;
+                    }
+                    if (metadeB < 1)
+                        break;
+                }
+                contador++;
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Versao2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Versao2
+{
+    internal class Program
+    {
+        static void Main(string[] args) {
+            int x, quantidadeX = 1, quantidadeIteraçao = 0;
+            bool alternar = false;
+            while (true) {
+                Console.WriteLine("Digite um valor que será utilizado para calcular o tamanho do diamante");
+                if (!int.TryParse(Console.ReadLine(), out x) || x % 2 == 0) {
+                    Console.WriteLine("Digite apenas números e com a condição de que sejam ímpares !!");
+                    continue;
+                }
+                break;
+            }
+            int quantidadeEspaço = x / 2;
+            for (int i = 0; i < Math.Sqrt(x); i++) {
+                if (i % x == 0) {
+                    quantidadeIteraçao++;
+                    if (quantidadeX > 0)
+                        Console.Write("X");
+                    else
+                       Console.Write(" ");
+                    Console.WriteLine();
+                    quantidadeX += 1 + quantidadeIteraçao*2;
+                    quantidadeEspaço = x / 2 - quantidadeIteraçao;
+                    continue;
+                }
+                if (quantidadeEspaço > 0 ) {
+                    Console.Write(" ");
+                    quantidadeEspaço--;
+                }  
+                else {
+                    if (quantidadeX > 0 ) {                    
+                        Console.Write("X");
+                        quantidadeX--;
+                    }
+                    else {
+                        quantidadeEspaço = x / 2 - quantidadeIteraçao;
+                    }
+                }
+            }
+        }
+    }
+}
